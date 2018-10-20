d2dbb9f0ac9116d3b7eb164801c25d095820188e
karadjordje <sveljod@gmail.com>
2018-10-20 22:03:56 +0200

    Basic logic added

--- src/App.js
+++ src/App.js
@@ -5,76 +5,118 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			status: 'Unlocked',
+			interface: '',
 		};
+
+		this.handleBtnClick = this.handleBtnClick.bind(this);
+		this.idleUser = this.idleUser.bind(this);
+	}
+
+	handleBtnClick(e) {
+		console.log(e.target.value);
+		let btnVal = e.target.value;
+		let interfaceVal = this.state.interface + btnVal;
+		this.setState({
+			interface: interfaceVal,
+		});
+	}
+
+	idleUser() {
+		let time;
+		let screen = document.querySelector('.screen');
+		let btns = document.querySelectorAll('button');
+
+		window.onload = resetTimer;
+		btns.forEach(btn => {
+			btn.addEventListener('click', function() {
+				resetTimer();
+			});
+		});
+
+		function turnOffScreen() {
+			screen.style.background = '#47b2b2';
+		}
+
+		function resetTimer() {
+			if (screen.style.background === 'rgb(71, 178, 178)') {
+				screen.style.background = '#7fffff';
+			}
+			clearTimeout(time);
+			time = setTimeout(turnOffScreen, 5000);
+		}
+	}
+
+	componentDidMount() {
+		this.idleUser();
 	}
 
 	render() {
 		return (
-			<div class="panel">
-				<div class="screen">
-					<h1 class="value">Record</h1>
-					<h3 class="status">Locked</h3>
+			<div className="panel">
+				<div className="screen">
+					<input type="text" className="status" defaultValue={this.state.status} />
+					<input type="text" className="value" defaultValue={this.state.interface} />
 				</div>
-				<div class="buttons_parent">
-					<div class="button_group">
-						<button class="custom_btn">
+				<div className="buttons_parent">
+					<div className="button_group">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="7">
 							7
 						</button>
-						<button class="custom_btn">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="8">
 							8
-							<span class="bottom_right">
-								<i class="fas fa-arrow-up"></i>
+							<span className="bottom_right">
+								<i className="fas fa-arrow-up"></i>
 							</span>
 						</button>
-						<button class="custom_btn">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="9">
 							9
 						</button>
 					</div>
-					<div class="button_group">
-						<button class="custom_btn">
+					<div className="button_group">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="4">
 							4
-							<span class="bottom_right">
-								<i class="fas fa-arrow-left"></i>
+							<span className="bottom_right">
+								<i className="fas fa-arrow-left"></i>
 							</span>
 						</button>
-						<button class="custom_btn">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="5">
 							5
 						</button>
-						<button class="custom_btn">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="6">
 							6
-							<span class="bottom_right">
-								<i class="fas fa-arrow-right"></i>
+							<span className="bottom_right">
+								<i className="fas fa-arrow-right"></i>
 							</span>
 						</button>
 					</div>
-					<div class="button_group">
-						<button class="custom_btn">
+					<div className="button_group">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="1">
 							1
 						</button>
-						<button class="custom_btn">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="2">
 							2
-							<span class="bottom_right">
-								<i class="fas fa-arrow-down"></i>
+							<span className="bottom_right">
+								<i className="fas fa-arrow-down"></i>
 							</span>
 						</button>
-						<button class="custom_btn">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="3">
 							3
 						</button>
 					</div>
-					<div class="button_group">
-						<button class="custom_btn">
+					<div className="button_group">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="R">
 							R
-							<span class="bottom_right">
+							<span className="bottom_right">
 								B
 							</span>
 						</button>
-						<button class="custom_btn">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="0">
 							0
 						</button>
-						<button class="custom_btn">
+						<button onClick={this.handleBtnClick} className="custom_btn" value="L">
 							L
-							<span class="bottom_right">
+							<span className="bottom_right">
 								A
 							</span>
 						</button>
