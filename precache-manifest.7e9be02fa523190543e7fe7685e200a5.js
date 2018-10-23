386729ba8d700f03fa395944b3016e29b3f86c29
karadjordje <sveljod@gmail.com>
2018-10-23 22:19:50 +0200

    Updates

---
+++ precache-manifest.7e9be02fa523190543e7fe7685e200a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e93c46bc6c6da62afacc",
+    "url": "/deposit-safe/static/js/runtime~main.e93c46bc.js"
+  },
+  {
+    "revision": "f26370424cf54d826f8c",
+    "url": "/deposit-safe/static/js/main.f2637042.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/deposit-safe/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "f26370424cf54d826f8c",
+    "url": "/deposit-safe/static/css/main.3e6d3be2.chunk.css"
+  },
+  {
+    "revision": "e0833c2765841888cbf52696844738a3",
+    "url": "/deposit-safe/index.html"
+  }
+];
